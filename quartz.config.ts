55a93d87444796cabb6b478ff710f871d6a0a459
YiHsin-Tsai <elijah.yihsin.tsai@gmail.com>
2026-04-22 22:24:11 +0800

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://yihsin-tsai.github.io/quartz",
+    baseUrl: "yihsin-tsai.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
