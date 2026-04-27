4c31273aa3c664fd0cf5ea0477d3c7bbedf9e1a3
YiHsin-Tsai <elijah.yihsin.tsai@gmail.com>
2026-04-27 20:07:12 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Yi-Hsin Tsai",
-    pageTitleSuffix: "Yi-Hsin Tsai",
+    pageTitleSuffix: " | Yi-Hsin Tsai",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
